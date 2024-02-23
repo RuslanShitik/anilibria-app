c265839e04f768275f19acef0c43fea7e8673bdd
Ruslan <RuslanShitik@yandex.by>
2024-02-24 00:30:06 +0300

    feat: prepare to add video

--- src/composables/urls.ts
+++ src/composables/urls.ts
@@ -1,3 +1,6 @@
-export const getImageUrl = (url: string):string => {
-    return 'https://www.anilibria.tv/' + url || ''
-}
+export const getImageUrl = (url: string): string => {
+  return 'https://www.anilibria.tv/' + url || '';
+};
+export const getVideoUrl = (url: string): string => {
+  return 'https://cache.libria.fun' + url || '';
+};
--- src/pages/TitlePage.vue
+++ src/pages/TitlePage.vue
@@ -1,5 +1,12 @@
 <template>
-  <page-component> anime: {{ titleData }} </page-component>
+  <PageComponent>
+    <!-- <video
+      v-if="titleData"
+      :src="getVideoUrl(titleData.player.list[1].hls.sd)"
+    ></video> -->
+
+    anime: {{ titleData }}
+  </PageComponent>
 </template>
 
 <script setup lang="ts">
@@ -9,16 +16,17 @@ import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useQuasar } from 'quasar';
 import { titleT } from 'src/types';
+import { getVideoUrl } from 'src/composables/urls';
+
 const $q = useQuasar();
 
 const { params } = useRoute();
 const code = String(params.code);
-const titleData = ref<titleT | null>()
+const titleData = ref<titleT | null>();
 
-onMounted(async() => {
-    $q.loading.show();
-    titleData.value = await getTitleByCode(code)
-    $q.loading.hide();
-})
-
+onMounted(async () => {
+  $q.loading.show();
+  titleData.value = await getTitleByCode(code);
+  $q.loading.hide();
+});
 </script>
